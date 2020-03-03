d04ec85e3b34e1bef44544a5d70f7a04657f2605
Braam Beresford <braamberesford@10-248-209-198.wireless.oregonstate.edu>
2020-03-02 19:03:28 -0800

    Created charge batt state function with basic state
    Co-authored-by: Bao Nguyen <baonguyen.vincent@gmail.com>
    Co-authored-by: 17brownj1 <brownjo3@oregonstate.edu>

--- ChargeController.ino
+++ ChargeController.ino
@@ -4,6 +4,9 @@
 
 // Data wire is plugged into digital pin D6 on the Arduino
 #define ONE_WIRE_BUS 6
+#define CONSTANT_CHARGING_VOLTAGE 14.2
+#define CONSTANT_CHARGING_CURRENT 2
+#define FLOATING_BATT_VOLTAGE 13.5
 
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);	
@@ -31,17 +34,31 @@ const int currentLoadPin 	= A6;
 
 // Global vars
 
+//Open Current
 float vOpen;
+
+//Solar panel short current
 float iShort; 
-float votlageFromPanel;
+
+//Current voltage from panel
+float voltageFromPanel;
+
+//Immediate current from panel
 float currentFromPanel;
-float Pcurr;
+
+//
 float currentToSystem;
+//hai
+
 float voltageToBatt;
 float currentToBatt;         // = CurrentToSystem - CurrentToLoad
 float currentToLoad;         //This is the current of load
 
 byte wiper;
+
+typedef enum BattStates {BULK,TRICKLE, FlOATING} BattStates;
+BattStates BattState = BULK;
+
 
 void setup() {
   pinMode(solarShortPin, INPUT);		//Default OFF, there is pull down resistor
@@ -57,6 +74,7 @@ void setup() {
   
   // turn on LiIon charger
   digitalWrite(liIonTempLEDPin, LOW);
+  digitalWrite(enLiIonPin, LOW);
   
   // declare global variable
   wiper = 128;   //init
@@ -67,6 +85,12 @@ void setup() {
   temperatureSensor.begin();
 }
 
+
+/*
+ * name: changePot()
+ * return: void
+ * Pretty code - I2C communication to change feedback voltage
+ */
 void changePot(byte newWiperValue){
   //wiper value should be a number between 0 and 256, because there are 255 positions on the pot. The value of the pot can be measured from the wiper to point A or B.
   //Measuring from point A to the wiper will cause the resistance of the pot to decrease as the decimal value sent to it increases. Measuring from B to the wiper will cause the opposite to happen, but in this code we will 
@@ -78,10 +102,33 @@ void changePot(byte newWiperValue){
   Wire.write(newWiperValue);//the third byte is the decimal value used to determine the resistance. This value should be between 0 and 255
   Wire.endTransmission();
 
-  delay(500);
 }
 
-int readValues(){//returns the state of the circuit and how the battery should be charged. 
+/*
+ * bulkChangingBangBang() 
+ * return: void
+ * Description: Bumps current until hits 2 amps
+ */
+void bulkChangingBangBang(){
+  //do I need to shut off the power to the battery to take an accurate reading of its resting voltage. 
+  if(currentToBatt > 2){
+    wiper++;              //CHECK if this decrease charging voltage;
+    changePot(wiper);
+  }
+
+  else{
+    wiper--;
+    changePot(wiper);
+  }
+}
+
+
+/*
+ * name: readValues()
+ * return: void
+ * description: Read in analog values and update to global vars
+ */
+void readValues(){//returns the state of the circuit and how the battery should be charged. 
    int currentPanelTemporary      = analogRead(currentInPin);   //from solar
    int voltageFromPanelTemporary  = analogRead(voltageInPin);   //from solar
    int currentOutTemporary        = analogRead(currentOutPin);  // Ibatt + Iload
@@ -91,63 +138,27 @@ int readValues(){//returns the state of the circuit and how the battery should b
 
     //scale all of our values back
     // Update Global Vars
-    currentFromPanel = (currentPanelTemporary*5/1024)/.110;
-    currentToSystem = (currentOutTemporary*5/1024)/.110;
-    currentToLoad = (currentLoadTemporary*5/1024)/.110;
-    currentToBatt = currentToSystem - currentToLoad;
-    voltageToBatt = voltageToBatteryTemporary *.01543;
-
-  //  currentFromPanel = (currentPanelTemporary/2)/.110;
-  //  float currentToSystem = (currentOutTemporary/2)/.110;
-  //  float currentToLoad = (currentLoadTemporary/2)/.110;
-  //  float totalCurrentRequired = currentToBattery + currentToLoad; //CHECK
-  //  float batteryVoltage = voltageToBattery * .01543;
+    currentFromPanel  = -((currentPanelTemporary*5.0/1024)-2.55)/0.165;
+    currentToSystem   = -((currentOutTemporary*5.0/1024)-2.55)/0.165;
+    currentToLoad     = -((currentLoadTemporary*5.0/1024)-2.55)/0.165;
+    currentToBatt     = currentToSystem - currentToLoad;
+    voltageToBatt     = voltageToBatteryTemporary *.01543;
 
 
-   //for debug
-   Serial.print(currentFromPanel);
-   Serial.print(currentToSystem);
-   Serial.print(currentToLoad);
-   Serial.print(currentToBatt);
-   Serial.print(voltageToBatt);
+  //TODO: Remove later
+  //for debug
+  //  Serial.print(currentFromPanel);
+  //  Serial.print(currentToSystem);
+  //  Serial.print(currentToLoad);
+  //  Serial.print(currentToBatt);
+  //  Serial.print(voltageToBatt);
    
-//   if(batteryVoltage <13.4){
-//    //bulk charge
-//    return 1;
-//   }
-//
-//   else if(batteryVoltage > 14.2){
-//    //rest
-//    return 2;
-//   }
-//
-//   else if (batteryVoltage > 13.2){
-//    return 3; 
-//   }
-//   
-//   else {
-//    return 4;
-//   }
-//   
+ 
+  
    
 }
 
-void bulkChangingBangBang(){
-  do{    
-    if(currentToBatt > 1.5){
-      wiper++;              //CHECK if this decrease charging voltage;
-      changePot(wiper);
-    }
 
-    else{
-      wiper--;
-      changePot(wiper);
-    }
-    delay(500);
-    //do I need to shut off the power to the battery to take an accurate reading of its resting voltage. 
-  } while(voltageToBatt < 14.2);
-
-}
 
 /*
  * tempSensingAndShutoff()
@@ -177,20 +188,25 @@ void tempSensingAndShutoff(){
    }
   
     else{
-	  //Turn off LED if nothing is wrong
+	    //Turn off LED if nothing is wrong
       digitalWrite(systemTempLEDPin, LOW);
       //Serial.print("skip broken led");
    }
    
     if(overTempLiIon = 1){
-	   //turn off charging until reset
+	    //turn off charging until reset
 	    digitalWrite(enLiIonPin, HIGH);
 	    digitalWrite(liIonTempLEDPin, HIGH);
    }   
 
 }
 
-
+/*
+ * shortCurrentOpenVoltage()
+ * return: void
+ * Turn of regulator to measure Vopen, then short circuit to measure Ishort
+ * CHECK: Do the system need to recover?
+ */
 void shortCurrentOpenVoltage(){ //sends the status of the panel and how much power it is producing
    //Perform shutdown voltage regulator
     pinMode(solarEnPin, OUTPUT);
@@ -201,7 +217,7 @@ void shortCurrentOpenVoltage(){ //sends the status of the panel and how much pow
     pinMode(solarShortPin, OUTPUT);
     digitalWrite(solarShortPin, HIGH);
     delay(100);
-    iShort = (analogRead(currentInPin)/2)/.110;
+    iShort = (-((analogRead(currentInPin)*5.0/1024)-2.55)/0.165);
    
     //Turn off short MOSFET
     //Turn voltage regulator back on
@@ -222,34 +238,15 @@ void sendShortOpen(){
  */
 void sendChargingData(){
   //  int value = 0;
-  Serial.println(votlageFromPanel);
+  Serial.println(voltageFromPanel);
   Serial.println(currentFromPanel);
-  Serial.println(votlageFromPanel*currentFromPanel); // power from panel
-
-  //Serial.println(batteryVoltage);
-  //Serial.println(currentToBatt);
-  //Serial.println(currentToLoad);
-
-  
-  //  value = analogRead(currentOut);
-  //  Serial.write(value);
-  //  value = analogRead(voltageBattPin);
-  //  Serial.write(value); 
+  Serial.println(voltgeFromPanel*currentFromPanel); // power from panel
 }
 
-/*
- * Need to rewrite to async code CHECK
- */
-void battFullWait10Mins(){
-  uint32_t period = 10 * 60000L;       // 10 minutes
 
-  for( uint32_t tStart = millis();  (millis()-tStart) < period;){ //hang out in this loop for 10 minutes and just send the Pi the charging and panel data if it asks for it.
-    sendSystemInfo(); 					// Sending too much data!
-  }
-}
 
 /*
- * sendSystemInfo()
+ * name: sendSystemInfo()
  * return: void
  * print out to serial requested data Vcurr, Icurr, Pcurr, Vopen, Ishort
 */
@@ -257,55 +254,76 @@ void sendSystemInfo(){ //send the voltage and current the battery sees.
   //Serial.begin(9600);
   int requestCode = 0;
   if(Serial.available() > 0){
-	requestCode = (Serial.read() - '0');
-	delay(50);
+    requestCode = (Serial.read() - '0');
+    delay(50);
 
     if(requestCode == 0){		//Code 0 mean regular charge data Vcurr, Icurr, Pcurr
-       sendChargingData();
+      sendChargingData();
     }
 
     else if(requestCode == 1){	//Code 1 mean perfrom Open-Short test for solar panel
-       shortCurrentOpenVoltage();
-       sendShortOpen();
+      shortCurrentOpenVoltage();
+      sendShortOpen();
     }
 
     else{ //something was received through serial that wasn't one of those 2 strings. 
-       Serial.print(requestCode);
+      //  Serial.print(requestCode);
     } 
   }
 }
 
-void loop() {
-  sendSystemInfo();
-  int state = readValues();
-  //Gather data
-
-  //Execute based on state
-
-  if(state == 1){
- 
+/*
+ * name: sendSystemInfo()
+ * return: void
+ * print out to serial requested data Vcurr, Icurr, Pcurr, Vopen, Ishort
+*/
+void setBattVoltageBangBang(float target){
+  //As wiper increases votage decreases
+  if(voltageToBatt > target){
+    wiper++;              //CHECK if this decrease charging voltage;
+    changePot(wiper);
   }
 
-  else{
-       /*
-    
-  
-    switch(state)
-      case 1:
-       //blah
-       //change pot value
-       break;
-  
-      case 2:
-        break;
-  
-      default:
-        //minimum charging voltage
-        break;
+  else if (voltageToBatt < target){
+    wiper--;
+    changePot(wiper);
+  }
+
+}
+
+//Handles logic for chargin battery
+//TODO: Handle moving backward in states
+void chargeBatt(){
+  //If under chanrged
+  switch(BattState){
+    case BULK:
+      bulkChangingBangBang();
+      if(voltageToBatt > CONSTANT_CHARGING_VOLTAGE){
+        BattState = TRICKLE;
       }
-      */
+    break;
+    case TRICKLE:
+      if(currentToBatt <= 0.2 * CONSTANT_CHARGING_CURRENT){
+        BattState = FlOATING;
+      }
+      
+    break;  
+
+    case FlOATING:
+      setBattVoltageBangBang(FLOATING_BATT_VOLTAGE)
+
+    break;
   }
 
+
+}
+
+
+void loop() {
+  tempSensingAndShutoff();
+
+  sendSystemInfo();
+  chargeBatt();
   
   
 }
