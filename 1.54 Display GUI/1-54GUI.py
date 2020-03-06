b7835b6c3c288865adc5848e73c2c425006f1397
BaoNguyen <baonguyen.vincent@gmail.com>
2020-03-06 15:07:30 -0800

    Add tab font and increase text size from 15 to 35

--- 1.54 Display GUI/1-54GUI.py
+++ 1.54 Display GUI/1-54GUI.py
@@ -13,6 +13,9 @@ class App(Tk):
         self.minsize(600, 400)
         self.config(bg='white')
 
+        #change tab font
+        self.s= ttk.Style()
+        self.s.configure('TNotebook.Tab', font=('Helvetica','30'))
         tabControl = ttk.Notebook(self)
         self.tab1 = ttk.Frame(tabControl)
         tabControl.add(self.tab1, text="Solar")
@@ -85,37 +88,37 @@ class App(Tk):
         labelFrame.grid(column=0, row=0, padx=8, pady=4)
         labelFrame.config(bg='white')
 
-        vOpenText = Label(labelFrame, text="V Open: ", fg ="#333", bg = "#fff",font=('Helvetica', '15')).grid(column=0, row = 0, sticky = W)
-        self.vOpenDis = Label(labelFrame, text="1", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        vOpenText = Label(labelFrame, text="V Open: ", fg ="#333", bg = "#fff",font=('Helvetica', '35')).grid(column=0, row = 0, sticky = W)
+        self.vOpenDis = Label(labelFrame, text="1", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.vOpenDis.grid(column=1, row=0, sticky = W)                                     
 
-        iShortText = Label(labelFrame, text="I Short: ", fg ="#333", bg = "#fff",font=('Helvetica', '15')).grid(column=0, row = 1, sticky = W) 
-        self.iShortDis = Label(labelFrame, text="2", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        iShortText = Label(labelFrame, text="I Short: ", fg ="#333", bg = "#fff",font=('Helvetica', '35')).grid(column=0, row = 1, sticky = W) 
+        self.iShortDis = Label(labelFrame, text="2", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.iShortDis.grid(column=1, row=1, sticky = W)
 
-        vCurrText = Label(labelFrame, text="V Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 2, sticky = W)
-        self.vCurrDis = Label(labelFrame, text="3", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        vCurrText = Label(labelFrame, text="V Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 2, sticky = W)
+        self.vCurrDis = Label(labelFrame, text="3", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.vCurrDis.grid(column=1, row=2, sticky = W)
 
-        iCurrText = Label(labelFrame, text="I Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 3, sticky = W)
-        self.iCurrDis = Label(labelFrame, text="4", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        iCurrText = Label(labelFrame, text="I Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 3, sticky = W)
+        self.iCurrDis = Label(labelFrame, text="4", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.iCurrDis.grid(column=1, row=3, sticky = W)
 
-        pCurrText = Label(labelFrame, text="P Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 4, sticky = W)
-        self.pCurrDis = Label(labelFrame, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        pCurrText = Label(labelFrame, text="P Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 4, sticky = W)
+        self.pCurrDis = Label(labelFrame, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.pCurrDis.grid(column=1, row=4, sticky = W)
 
-        vSymbol = lambda:Label(labelFrame, text="V", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        vSymbol = lambda:Label(labelFrame, text="V", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         vSymbol().grid(column=3, row=0)
         vSymbol().grid(column=3, row=2)
 
-        iSymbol = lambda:Label(labelFrame, text="A", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        iSymbol = lambda:Label(labelFrame, text="A", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         iSymbol().grid(column=3, row=1)
         iSymbol().grid(column=3, row=3)
 
-        pSymbol = Label(labelFrame, text="W", fg ="#333", bg = "#fff",font=('Helvetica', '15')).grid(column=3, row=4)
+        pSymbol = Label(labelFrame, text="W", fg ="#333", bg = "#fff",font=('Helvetica', '35')).grid(column=3, row=4)
         # Button here
-        btn = Button(labelFrame, text="Reset Solar Info", width = 15, height= 2, command=self.getShortOpen, border="0", bg="#E85B2A", activebackground = "#fff", activeforeground = "#E85B2A", fg = "#fff", font=('Helvetica', '15'))
+        btn = Button(labelFrame, text="Reset Solar Info", width = 14, height= 2, command=self.getShortOpen, border="0", bg="#E85B2A", activebackground = "#fff", activeforeground = "#E85B2A", fg = "#fff", font=('Helvetica', '35'))
         btn.grid(column=0, columnspan=2, row = 5, padx=12, sticky = EW)
         # Basic layout end
 
@@ -126,37 +129,37 @@ class App(Tk):
         labelFrame2.config(bg='white')
         labelFrame2.grid(column=0, row=0, padx=8, pady=4)
         
-        voltageBatText = Label(labelFrame2, text="Battery Voltage: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 0, sticky = W)
-        self.voltageBatDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        voltageBatText = Label(labelFrame2, text="Battery Voltage: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 0, sticky = W)
+        self.voltageBatDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.voltageBatDis.grid(column=1, row=0, sticky = W)
 
-        currentBatText = Label(labelFrame2, text="Battery Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 1, sticky = W)
-        self.currentBatDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        currentBatText = Label(labelFrame2, text="Battery Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 1, sticky = W)
+        self.currentBatDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.currentBatDis.grid(column=1, row=1, sticky = W)
 
-        currentLoadText = Label(labelFrame2, text="Load Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 2, sticky = W)
-        self.currentLoadDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        currentLoadText = Label(labelFrame2, text="Load Current: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 2, sticky = W)
+        self.currentLoadDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.currentLoadDis.grid(column=1, row=2, sticky = W)        
         
-        internalTempText = Label(labelFrame2, text="Internal Temp: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 3, sticky = W)
-        self.internalTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        internalTempText = Label(labelFrame2, text="Internal Temp: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 3, sticky = W)
+        self.internalTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.internalTempDis.grid(column=1, row=3, sticky = W)
 
-        leadAcidTempText = Label(labelFrame2, text="Lead-Acid Temp: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 4, sticky = W)
-        self.leadAcidTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        leadAcidTempText = Label(labelFrame2, text="Lead-Acid Temp: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 4, sticky = W)
+        self.leadAcidTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.leadAcidTempDis.grid(column=1, row=4, sticky = W)
 
-        liIonTempText = Label(labelFrame2, text="Li-ion Temp: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 5, sticky = W)
-        self.liIonTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        liIonTempText = Label(labelFrame2, text="Li-ion Temp: ", fg ="#333", bg = "#fff", font=('Helvetica', '35')).grid(column=0, row = 5, sticky = W)
+        self.liIonTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         self.liIonTempDis.grid(column=1, row=5, sticky = W)
 
-        VSymbol = Label(labelFrame2, text="V", fg ="#333", bg = "#fff",font=('Helvetica', '15')).grid(column=3, row=0)
+        VSymbol = Label(labelFrame2, text="V", fg ="#333", bg = "#fff",font=('Helvetica', '35')).grid(column=3, row=0)
 
-        iSymbol = lambda:Label(labelFrame2, text="A", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        iSymbol = lambda:Label(labelFrame2, text="A", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         iSymbol().grid(column=3, row=1)
         iSymbol().grid(column=3, row=2)
 
-        CSymbol = lambda:Label(labelFrame2, text="°C", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
+        CSymbol = lambda:Label(labelFrame2, text="°C", fg ="#333", bg = "#fff", font=('Helvetica', '35'))
         CSymbol().grid(column=3, row=3)
         CSymbol().grid(column=3, row=4)
         CSymbol().grid(column=3, row=5)
