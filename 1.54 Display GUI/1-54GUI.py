f202e19191e984bba1795db6df3db5a100447985
BaoNguyen <baonguyen.vincent@gmail.com>
2020-03-05 21:28:24 -0800

    Added basic display location for V,I and Temp

--- 1.54 Display GUI/1-54GUI.py
+++ 1.54 Display GUI/1-54GUI.py
@@ -12,10 +12,10 @@ class App(Tk):
 
         tabControl = ttk.Notebook(self)
         self.tab1 = ttk.Frame(tabControl)
-        tabControl.add(self.tab1, text="Tab 1")
+        tabControl.add(self.tab1, text="Solar")
 
         self.tab2 = ttk.Frame(tabControl)
-        tabControl.add(self.tab2, text="Tab 2")
+        tabControl.add(self.tab2, text="Temperature")
         tabControl.pack(expand=1, fill="both")
         self.widgets()
         self.attributes("-fullscreen", True)
@@ -25,24 +25,52 @@ class App(Tk):
     def widgets(self):
         labelFrame = LabelFrame(self.tab1, text="")
         labelFrame.grid(column=0, row=0, padx=8, pady=4)
-        label = Label(labelFrame, text="Enter Your Name:")
-        label.grid(column=0, row=0, sticky='W')
-        vOpenText = Label(labelFrame, text="V-Open: ", fg="#333")
-        vOpenText.grid(column=0, row=1, sticky=W)
-        vOpenDis = Label(labelFrame, text="1", fg="#333")
-        vOpenDis.grid(column=1, row=2, sticky=W)
+
+        vOpenText = Label(labelFrame, text="V-Open: ", fg ="#333", bg = "#fff").grid(column=0, row = 0, sticky = W)
+        vOpenDis = Label(labelFrame, text="1", fg ="#333", bg = "#fff")
+        vOpenDis.grid(column=1, row=0, sticky = W)                                     
+
+        iShortText = Label(labelFrame, text="I-Short: ", fg ="#333", bg = "#fff").grid(column=0, row = 1, sticky = W) 
+        iShortDis = Label(labelFrame, text="2", fg ="#333", bg = "#fff")
+        iShortDis.grid(column=1, row=1, sticky = W)
+
+        vCurrText = Label(labelFrame, text="V-Curr: ", fg ="#333", bg = "#fff").grid(column=0, row = 2, sticky = W)
+        vCurrDis = Label(labelFrame, text="3", fg ="#333", bg = "#fff")
+        vCurrDis.grid(column=1, row=2, sticky = W)
+
+        iCurrText = Label(labelFrame, text="I-Curr: ", fg ="#333", bg = "#fff").grid(column=0, row = 3, sticky = W)
+        iCurrDis = Label(labelFrame, text="4", fg ="#333", bg = "#fff")
+        iCurrDis.grid(column=1, row=3, sticky = W)
+
+        pCurrText = Label(labelFrame, text="P-now: ", fg ="#333", bg = "#fff").grid(column=0, row = 4, sticky = W)
+        pCurrDis = Label(labelFrame, text="5", fg ="#333", bg = "#fff")
+        pCurrDis.grid(column=1, row=4, sticky = W)
+
+        vSymbol = lambda:Label(labelFrame, text="V", fg ="#333", bg = "#fff")
+        vSymbol().grid(column=3, row=0)
+        vSymbol().grid(column=3, row=2)
+
+        iSymbol = lambda:Label(labelFrame, text="I", fg ="#333", bg = "#fff")
+        iSymbol().grid(column=3, row=1)
+        iSymbol().grid(column=3, row=3)
+
+        pSymbol = Label(labelFrame, text="W", fg ="#333", bg = "#fff").grid(column=3, row=4)
 
 # This is the 2nd tab for the shit
-        labelFrame2 = LabelFrame(self.tab2, text="Second Tab")
+        labelFrame2 = LabelFrame(self.tab2, text="")
         labelFrame2.grid(column=0, row=0, padx=8, pady=4)
-        label = Label(labelFrame2, text="Enter Your Name:")
-        label.grid(column=0, row=0, sticky='W')
-        textEdit = Entry(labelFrame2, width=20)
-        textEdit.grid(column=1, row=0)
-        label2 = Label(labelFrame2, text="Enter Your Password:")
-        label2.grid(column=0, row=1)
-        textEdit = Entry(labelFrame2, width=20)
-        textEdit.grid(column=1, row=1)
+
+        internalTempText = Label(labelFrame2, text="Internal Temp: ", fg ="#333", bg = "#fff").grid(column=0, row = 0, sticky = W)
+        internalTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff")
+        internalTempDis.grid(column=1, row=0, sticky = W)
+
+        leadAcidTempText = Label(labelFrame2, text="LeadAcid Temp: ", fg ="#333", bg = "#fff").grid(column=0, row = 1, sticky = W)
+        leadAcidTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff")
+        leadAcidTempDis.grid(column=1, row=1, sticky = W)
+
+        liIonTempText = Label(labelFrame2, text="Li-ion Temp: ", fg ="#333", bg = "#fff").grid(column=0, row = 2, sticky = W)
+        liIonTempDis = Label(labelFrame2, text="5", fg ="#333", bg = "#fff")
+        liIonTempDis.grid(column=1, row=2, sticky = W)
 
 
 app = App()
