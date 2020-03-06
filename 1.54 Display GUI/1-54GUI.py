e4517f183248ca5dffc0934a5290c90dd1635746
BaoNguyen <baonguyen.vincent@gmail.com>
2020-03-05 21:41:11 -0800

    Add button and lager font

--- 1.54 Display GUI/1-54GUI.py
+++ 1.54 Display GUI/1-54GUI.py
@@ -21,43 +21,53 @@ class App(Tk):
         self.attributes("-fullscreen", True)
         
     
-    
+    def getShortOpen(self):
+        print("HI")    
+
     def widgets(self):
+        ## Tab1 start here
+        # Basic layout start
         labelFrame = LabelFrame(self.tab1, text="")
         labelFrame.grid(column=0, row=0, padx=8, pady=4)
         labelFrame.config(bg='white')
 
-        vOpenText = Label(labelFrame, text="V-Open: ", fg ="#333", bg = "#fff").grid(column=0, row = 0, sticky = W)
-        vOpenDis = Label(labelFrame, text="1", fg ="#333", bg = "#fff")
+        vOpenText = Label(labelFrame, text="V-Open: ", fg ="#333", bg = "#fff",font=('Helvetica', '15')).grid(column=0, row = 0, sticky = W)
+        vOpenDis = Label(labelFrame, text="1", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
         vOpenDis.grid(column=1, row=0, sticky = W)                                     
 
-        iShortText = Label(labelFrame, text="I-Short: ", fg ="#333", bg = "#fff").grid(column=0, row = 1, sticky = W) 
-        iShortDis = Label(labelFrame, text="2", fg ="#333", bg = "#fff")
+        iShortText = Label(labelFrame, text="I-Short: ", fg ="#333", bg = "#fff",font=('Helvetica', '15')).grid(column=0, row = 1, sticky = W) 
+        iShortDis = Label(labelFrame, text="2", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
         iShortDis.grid(column=1, row=1, sticky = W)
 
-        vCurrText = Label(labelFrame, text="V-Curr: ", fg ="#333", bg = "#fff").grid(column=0, row = 2, sticky = W)
-        vCurrDis = Label(labelFrame, text="3", fg ="#333", bg = "#fff")
+        vCurrText = Label(labelFrame, text="V-Curr: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 2, sticky = W)
+        vCurrDis = Label(labelFrame, text="3", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
         vCurrDis.grid(column=1, row=2, sticky = W)
 
-        iCurrText = Label(labelFrame, text="I-Curr: ", fg ="#333", bg = "#fff").grid(column=0, row = 3, sticky = W)
-        iCurrDis = Label(labelFrame, text="4", fg ="#333", bg = "#fff")
+        iCurrText = Label(labelFrame, text="I-Curr: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 3, sticky = W)
+        iCurrDis = Label(labelFrame, text="4", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
         iCurrDis.grid(column=1, row=3, sticky = W)
 
-        pCurrText = Label(labelFrame, text="P-now: ", fg ="#333", bg = "#fff").grid(column=0, row = 4, sticky = W)
-        pCurrDis = Label(labelFrame, text="5", fg ="#333", bg = "#fff")
+        pCurrText = Label(labelFrame, text="P-now: ", fg ="#333", bg = "#fff", font=('Helvetica', '15')).grid(column=0, row = 4, sticky = W)
+        pCurrDis = Label(labelFrame, text="5", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
         pCurrDis.grid(column=1, row=4, sticky = W)
 
-        vSymbol = lambda:Label(labelFrame, text="V", fg ="#333", bg = "#fff")
+        vSymbol = lambda:Label(labelFrame, text="V", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
         vSymbol().grid(column=3, row=0)
         vSymbol().grid(column=3, row=2)
 
-        iSymbol = lambda:Label(labelFrame, text="I", fg ="#333", bg = "#fff")
+        iSymbol = lambda:Label(labelFrame, text="I", fg ="#333", bg = "#fff", font=('Helvetica', '15'))
         iSymbol().grid(column=3, row=1)
         iSymbol().grid(column=3, row=3)
 
-        pSymbol = Label(labelFrame, text="W", fg ="#333", bg = "#fff").grid(column=3, row=4)
+        pSymbol = Label(labelFrame, text="W", fg ="#333", bg = "#fff",font=('Helvetica', '15')).grid(column=3, row=4)
 
-# This is the 2nd tab for the shit
+        btn = Button(labelFrame, text="Reset Solar Info", width = 15, height= 2, command=self.getShortOpen, border="0", bg="#E85B2A", activebackground = "#fff", activeforeground = "#E85B2A", fg = "#fff", font=('Helvetica', '15'))
+        btn.grid(column=0, columnspan=2, row = 5, padx=12, sticky = EW)
+        # Basic layout end
+
+
+        ## Tab2 start here
+        # Basic layout start
         labelFrame2 = LabelFrame(self.tab2, text="")
         labelFrame2.config(bg='white')
         labelFrame2.grid(column=0, row=0, padx=8, pady=4)
@@ -78,6 +88,8 @@ class App(Tk):
         CSymbol().grid(column=3, row=0)
         CSymbol().grid(column=3, row=1)
         CSymbol().grid(column=3, row=2)
+        # Basic layout end
+
 
 
 app = App()
